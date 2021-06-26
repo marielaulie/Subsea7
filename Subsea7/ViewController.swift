2ae485e963794726766909f643f2c736e4639498
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-06-26 15:05:42 +0200

    Opprettet app

---
+++ Subsea7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Subsea7
+//
+//  Created by Mariel Aulie Hinderaker on 26/06/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
